6954ce5680bc137edce7adf0a92b32e21d6d12d4
woffordCodesAdmin <wofford.codes@gmail.com>
2023-03-18 10:04:40 -0500

    mainSection:

---
+++ app.js
@@ -0,0 +1,31 @@
+
+
+const playAudioJ = ()=>{
+    const jkey = new Audio('chillout-lo-fi-trap-beat_180bpm_D_major.wav')
+    jkey.play()
+}
+
+const playAudioV = ()=>{
+    const jkey = new Audio('gloomy-trap-beat-haunting-melody_150bpm_G_minor.wav')
+    jkey.play()
+}
+
+const playAudioL = ()=>{
+    const jkey = new Audio('pulsating-vocal-strings-trap-melody_142bpm.wav')
+    jkey.play()
+}
+
+const playAudioI = ()=>{
+    const jkey = new Audio('sauce.wav')
+    jkey.play()
+}
+
+const playAudioVV = ()=>{
+    const jkey = new Audio('drumms.wav')
+    jkey.play()
+}
+
+const playAudioN = ()=>{
+    const jkey = new Audio('clap.wav')
+    jkey.play()
+}
